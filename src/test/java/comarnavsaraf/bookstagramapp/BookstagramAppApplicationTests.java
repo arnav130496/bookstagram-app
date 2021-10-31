517d19fb90a798f72472ad3be58c4c9d17cd713c
Arnav <arnav130496@gmail.com>
2021-10-31 21:21:47 +0530

    project init

---
+++ src/test/java/comarnavsaraf/bookstagramapp/BookstagramAppApplicationTests.java
@@ -0,0 +1,13 @@
+package comarnavsaraf.bookstagramapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstagramAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
